3e24934ed4da2b0d21fe8db4206e533e1feb59bd
esquaredsec <esquaredsec@gmail.com>
2020-10-18 22:38:42 -0400

    Pipeline Review Test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Atlanta, Esquaredsec is Here!!!'));
+app.get('/', (req, res) => res.send('Hello Atlanta, Esquaredsec is Here. He is the GOAT!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
